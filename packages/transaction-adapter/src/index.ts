d070c77d0d3ca7180b604fd51c72b6a981e90271
nghinv <nghinv@inpirelab.io>
2022-12-01 20:03:49 +0700

    refactor: export keyword functionos

--- packages/transaction-adapter/src/index.ts
+++ packages/transaction-adapter/src/index.ts
@@ -2,6 +2,6 @@ export * from "./base";
 export * from "./const";
 export * from "./enums";
 export * from "./types";
-export * as AptosTransaction from "./aptos/transaction";
-export * as SUITransaction from "./sui/transaction";
+export * as Aptos from "./aptos/transaction";
+export * as SUI from "./sui/transaction";
 export * from "./transaction";

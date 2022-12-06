40fde941006e2d0406e9d0c98958a0c47f39effb
nghinv <nghinv@inpirelab.io>
2022-12-07 04:53:02 +0700

    feat: export network config

--- packages/utilities-adapter/src/index.ts
+++ packages/utilities-adapter/src/index.ts
@@ -2,6 +2,7 @@ export * from "./const";
 export * from "./enums";
 export * from "./types";
 export * from "./HexString";
+export * from "./network";
 export * as Helper from "./helper";
 export * as AptosUtil from "./aptos";
 export * as SUIUtil from "./sui";
